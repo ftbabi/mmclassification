fb4c457e95c2a851aa67b062c4a4dd5cdc7fb119
Yidi Shao <ydshao@smail.nju.edu.cn>
2021-02-07 18:44:55 +0800

    Fix bug in config

--- configs/_base_/models/mobilenet_v3_small_imagenet.py
+++ configs/_base_/models/mobilenet_v3_small_imagenet.py
@@ -1,12 +1,11 @@
 # model settings
 model = dict(
     type='ImageClassifier',
-    backbone=dict(type='MobileNetV3', arch='small'),
+    backbone=dict(type='MobileNetv3', arch='small'),
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
-        type='ConvClsHead',
+        type='LinearClsHead',
         num_classes=1000,
-        in_channels=576,
-        mid_channels=1280,
+        in_channels=96,
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         topk=(1, 5)))
